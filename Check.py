915aa79a939a64fc91d713722ae0a8dd91c73eed
Muthuji <42665005+Muthuji@users.noreply.github.com>
2023-11-24 10:34:02 +0530

    Update Check.py

--- Check.py
+++ Check.py
@@ -1,5 +1,6 @@
 import smtplib
 import pricecheck
+from python_whatsapp_bot import Whatsapp
 # creates SMTP session
 
 s = smtplib.SMTP('smtp.gmail.com', 587)
@@ -23,3 +24,7 @@ s.sendmail(sender, rec, pricecheck.ans)
  
 # terminating the session
 s.quit()
+
+Token='874317060671237|JiPZzDGezmtM0oehEbzgQ2ux-vU'
+wa_bot = Whatsapp(number_id='8072407662' ,token=Token)
+wa_bot.send_message('mobile eg: 2348145xxxxx3', 'Your message here')
